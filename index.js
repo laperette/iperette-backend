ead79f1b59350c585c2b928576a40ef6ef6e4e85
Alexandre Behaghel <alexandre.behaghel@club-internet.fr>
2017-11-30 23:59:24 +0100

    port changed 3000 -> 8080

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000
+  port = process.env.PORT || 8080
 
 app.route('*').get((req, res) => {
   res.send('Hello World')
 })
+
 app.listen(port)
 
 console.log('iPerette RESTful API server started on: ' + port)
