4a8db4f313b7beeae52996a550d7875f00ee25be
Alexandre Behaghel <alexandre.behaghel@club-internet.fr>
2017-12-03 17:01:52 +0100

    port changed to 6545

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 8080
+  port = process.env.PORT || 6545
 
 app.route('*').get((req, res) => {
   res.send('Hello World')
